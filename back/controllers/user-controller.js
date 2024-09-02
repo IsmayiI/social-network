b9f5d4748dbfafda11dbdedca2cc56a18e42f15a
Ismayil <ismayilov_ismayil_97@mail.ru>
2024-09-02 15:54:16 +0400

    add login controller

--- back/controllers/user-controller.js
+++ back/controllers/user-controller.js
@@ -3,6 +3,7 @@ const { prisma } = require("../prisma/prisma-client.js")
 const bcrypt = require("bcrypt")
 const path = require("path")
 const fs = require("fs")
+const jwt = require('jsonwebtoken')
 
 const UserController = {
    register: async (req, res) => {
@@ -44,7 +45,35 @@ const UserController = {
 
       res.send(`${email}, ${password}, ${name}`)
    },
-   login: async () => { },
+   login: async (req, res) => {
+      const { email, password } = req.body
+
+      if (!email || !password) {
+         return res.status(400).json({ error: 'Все поля обязательны' })
+      }
+
+      try {
+         const user = await prisma.user.findUnique({ where: { email } })
+         if (!user) {
+            return res.status(400).json({ error: 'Неверный логин или пароль' })
+         }
+
+         const valid = await bcrypt.compare(password, user.password)
+
+         if (!valid) {
+            return res.status(400).json({ error: 'Неверный логин или пароль' })
+         }
+
+         const token = jwt.sign(({ userId: user.id }), process.env.SECRET_KEY)
+
+         res.json({ token })
+
+      } catch (err) {
+         console.error('login error', err)
+         res.status(500).json({ error: 'Internal server error' })
+      }
+
+   },
    getUserById: async () => { },
    updateUser: async () => { },
    current: async () => { },
